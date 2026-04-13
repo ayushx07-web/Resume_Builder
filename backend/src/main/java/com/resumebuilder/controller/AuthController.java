23abf069c844712a2fb13d06635798a1eab4c567
ayushx07-web <kandpalayush4@gmail.com>
2026-04-13 19:12:15 +0530

    fix: import Value annotation for AuthController

--- backend/src/main/java/com/resumebuilder/controller/AuthController.java
+++ backend/src/main/java/com/resumebuilder/controller/AuthController.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
